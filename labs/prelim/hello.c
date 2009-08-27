28f2523a5ebf5b4cd165adefcdd430e18a766fbf
Michael Lee <lee@soi2.org>
2009-08-27 00:55:46 -0500

    Adding preliminary lab

---
+++ labs/prelim/hello.c
@@ -0,0 +1,14 @@
+/* 
+ * hello.c - the quintessential hello world program
+ * 
+ * Modify the lines below with your own name and e-mail.
+ *
+ * names  = Clark Kent
+ * emails = ckent@iit.edu
+ */
+#include <stdio.h>
+
+int main () {
+  printf("Hello world!\n");
+  return 0;
+}
