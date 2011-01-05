02b0e91c2e7185fdffd2b5b3f682e6a330b1ec25
Michael Lee <lee@soi2.org>
2011-01-04 21:58:04 -0600

    Adding malloc tracefiles to lab repository

--- labs/5_malloclab/config.h
+++ labs/5_malloclab/config.h
@@ -12,7 +12,7 @@
  * This is the default path where the driver will look for the
  * default tracefiles. You can override it at runtime with the -t flag.
  */
-#define TRACEDIR "/var/lib/cs/cs351/malloc_traces/";
+#define TRACEDIR "./traces/";
 
 /*
  * This is the list of default tracefiles in TRACEDIR that the driver
